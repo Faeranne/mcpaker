a837943ef1974b2fe2b965dca58b664f9de705e3
Blixa Morgan <blixa@projectmakeit.com>
2014-05-28 12:55:00 -0500

    add addmod functions

--- manager.js
+++ manager.js
@@ -1,11 +1,18 @@
-var mod = require('./mod.js')
+var Mod = require('./mod.js')
 
 var addMod = function(opts){
 	console.log(opts);
-	var newMod = {}
-	newMod.file = "./"+opts.zip
-	mod.getModZip(newMod); 
-	console.log(newMod);
+	var mod = new Mod(opts.name,opts.file);
+	mod.getZip();
+	mod.getInfo();
+	if(mod.notForge){
+		console.log("Not a forge mod. Only forge mods are supported at this time.");
+		return 1;
+	}
+	mod.getId();
+	mod.getVersio();
+	mod.getDeps();
+	console.log(mod);
 }
 
 module.exports.addMod = addMod;
